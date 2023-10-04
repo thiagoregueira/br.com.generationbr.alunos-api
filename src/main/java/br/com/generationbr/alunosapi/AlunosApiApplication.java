9a026b9a076c7c0d8d246e99220125e4822e17cb
Thiago Regueira <thiago.regueira@yahoo.com.br>
2023-10-04 15:53:25 -0300

    Corrigir problema de CORS no deploy

--- src/main/java/br/com/generationbr/alunosapi/AlunosApiApplication.java
+++ src/main/java/br/com/generationbr/alunosapi/AlunosApiApplication.java
@@ -3,6 +3,10 @@ package br.com.generationbr.alunosapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = @Server(url = "/", description = "Defautl Server URL"))
 @SpringBootApplication
 public class AlunosApiApplication {
 
